729342154316c19fd6d06863ed72f772c1cd3bc6
stanislasrigal <stanislas.rigal@laposte.net>
2022-03-02 17:28:48 +0100

    Use MSI and DFA results together.

--- MSI_MC.R
+++ MSI_MC.R
@@ -32,16 +32,51 @@ RES_forest <- MSI_MC_func(ts_forest)
 
 ggplot(RES_farmland, aes(x=year, y=MSI))+
   geom_point(colour = "dark green",size=3)+
-  ylim(70, NA)+ ylab("MSI (1996= 100 )")+
+  ylim(50, NA)+ ylab("MSI (1996= 100 )")+
   geom_ribbon(aes(ymin=lower_CL_trend, ymax=upper_CL_trend), alpha=0.2)+
   geom_line(aes(y=Trend), colour="dark green", size=1)+ theme_modern() +
   geom_pointrange(aes(ymax = MSI+sd_MSI, ymin=MSI-sd_MSI), colour="dark green")
 
 # MSI with DFA results
 farm_nfac3 <- readRDS("output/farm_nfac3.rds")
-farm_nfac3_reshape <- data.frame(code_sp=farm_nfac3[[1]]$code_sp,
-                                 year=farm_nfac3[[1]]$Year,
-                                 relative_abundance=farm_nfac3[[1]]$pred.value, # to multiply by sd and add mean orgi values
-                                 CI_inf=farm_nfac3[[1]]$pred.value-farm_nfac3[[1]]$pred_se.value*1.96)
+
+prep_data_msi <- function(x){
+  x <- droplevels(x)
+  x_reshape <- data.frame(code_sp=x$code_sp[1],
+                          year=c(1996,x$Year),
+                          relative_abundance=c(1,x$pred.value*sd(c(1,x$value_orig), na.rm=T)+mean(c(1,x$value_orig), na.rm=T)),
+                          CI_inf=c(1,x$pred.value*sd(c(1,x$value_orig), na.rm=T)+mean(c(1,x$value_orig), na.rm=T)-x$pred_se.value*1.96*sd(c(1,x$value_orig), na.rm=T)))
+  return(x_reshape)
+}
+
+farm_nfac3_reshape <- ddply(as.data.frame(farm_nfac3[[1]]), .(code_sp), .fun = prep_data_msi, .progress="text")
 
 RES_DFA_farmland <- MSI_MC_func(farm_nfac3_reshape)
+
+ggplot(RES_DFA_farmland, aes(x=year, y=MSI))+
+  geom_point(colour = "dark green",size=3)+
+  ylim(50, NA)+ ylab("MSI (1996 = 100 )")+
+  geom_ribbon(aes(ymin=lower_CL_trend, ymax=upper_CL_trend), alpha=0.2)+
+  geom_line(aes(y=Trend), colour="dark green", size=1)+ theme_modern() +
+  geom_pointrange(aes(ymax = MSI+sd_MSI, ymin=MSI-sd_MSI), colour="dark green")
+
+ggplot(RES_DFA_farmland, aes(x=year, y=MSI))+
+  geom_point(colour = "green",size=3)+
+  ylim(50, NA)+ ylab("MSI (1996 = 100 )")+
+  geom_ribbon(aes(ymin=lower_CL_trend, ymax=upper_CL_trend), alpha=0.2)+
+  geom_line(aes(y=Trend), colour="green", size=1)+ theme_modern() +
+  geom_pointrange(aes(ymax = MSI+sd_MSI, ymin=MSI-sd_MSI), colour="green")+
+  geom_point(data=RES_farmland,colour = "blue",size=3)+
+  geom_ribbon(data=RES_farmland,aes(ymin=lower_CL_trend, ymax=upper_CL_trend), alpha=0.2)+
+  geom_line(data=RES_farmland,aes(y=Trend), colour="blue", size=1)+ 
+  geom_pointrange(data=RES_farmland,aes(ymax = MSI+sd_MSI, ymin=MSI-sd_MSI), colour="blue")
+
+# Influence of each species on MSI
+
+mean_load <- as.data.frame(as.data.frame(farm_nfac3[[3]]) %>% 
+                             group_by(variable) %>% summarize(mean_load=mean(value)))
+
+trend_dfa <- as.matrix(dcast(as.data.frame(farm_nfac3[[2]])[,c("Year","variable","rot_tr.value")],
+                             Year~variable, value.var = "rot_tr.value"))
+
+msi_from_trend <- trend_dfa[,-1] %*% (matrix(mean_load$mean_load, ncol=1)/sum(matrix(mean_load$mean_load, ncol=1)))
